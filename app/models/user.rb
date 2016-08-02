b729d5a9a225a3a1abb7816a7984fb0f62ae64cd
Oscar Alvarez <oscar.alvarez.8830@gmail.com>
2016-08-02 16:53:50 -0400

    fix user session token validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -46,7 +46,7 @@ class User < ActiveRecord::Base
   private
 
   def ensure_session_token
-    self.session_token || User.generate_session_token
+    self.session_token ||= User.generate_session_token
   end
 
 end
