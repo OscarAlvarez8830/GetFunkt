157d8e6f95f430eb62a9cd70c5a883186fa998dd
Oscar Alvarez <oscar.alvarez.8830@gmail.com>
2016-08-03 14:54:47 -0400

    add auth buttons

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -14,17 +14,35 @@ const App = React.createClass({
   },
 
   greeting() {
-    // CHECK!!
+    // CHECK!! this isn't rendering the way I'd like
+    // logout button is showing up as soon as I render the login form
+    debugger
+    if ( !["/login", "/signup"].includes(this.props.location.pathname) ) {
+      return (
+        <nav className="login-signup">
+          <Link to="/login" activeClassName="current">Login</Link>
+          <Link to="/signup" activeClassName="current">Sign Up</Link>
+        </nav>
+      );
+    } else { // the problem is some unimplemented logic here, I'm sure
+      return (
+        <button onClick={this._handleLogOut}>Log Out</button>
+      );
+    }
   },
 
   render() {
     return (
       <div>
+        <section id="auth-buttons">
+          { this.greeting() }
+        </section>
+
         <header>
           <Link to="/" className="header-link"><h1>GetFunkt</h1></Link>
           <h4>Music For You, By You</h4>
         </header>
-        {this.props.children}
+        { this.props.children }
       </div>
     );
   }
